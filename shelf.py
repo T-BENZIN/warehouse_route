7df1485234688f521f476705282c57b8d713ce14
T-BENZIN <tbenzin1337@gmail.com>
2025-04-21 16:34:12 +0500

    + Создан класс полки который принимает адрес полки и рассчитывает её собственные координаты, координаты подбора (с прохода рядом с ней) и координаты верхнего и нижнего проходов на параллельные ряды. Сами методы по расчёту этих координат пока в стадии черновиков (не имеют самих расчётов и ничего не возвращают)

---
+++ shelf.py
@@ -0,0 +1,42 @@
+class Shelf:
+    """Класс определяющий полку и рассчитывающий её свойства"""
+    def __init__(self, address: str):
+        """
+        Инициализация экземпляра объекта.
+        По введённому адресу полки рассчитываются координаты подбора с полки и координаты проходов к другим рядам.
+        :param address: Адрес полки в текстовом формате. Например: 2/D/23
+        :type address: str
+        """
+        self.__address = address
+        self.__coordinates = self.coordinates_calculation()
+        self.__pickup_coordinates = self.pickup_coordinates_calculation()
+        self.__upper_passage_y_coordinate, self.__lower_passage_y_coordinate = self.passage_y_coordinates_calculation()
+
+    @property
+    def address(self) -> str:
+        return self.__address
+
+    @property
+    def coordinates(self) -> tuple[int, int]:
+        return self.__coordinates
+
+    @property
+    def pickup_coordinates(self) -> tuple[int, int]:
+        return self.__pickup_coordinates
+
+    @property
+    def upper_passage_y_coordinate(self) -> int:
+        return self.__upper_passage_y_coordinate
+
+    @property
+    def lower_passage_y_coordinate(self) -> int:
+        return self.__lower_passage_y_coordinate
+
+    def coordinates_calculation(self) -> tuple[int, int]:
+        pass
+
+    def pickup_coordinates_calculation(self) -> tuple[int, int]:
+        pass
+
+    def passage_y_coordinates_calculation(self) -> tuple[int, int]:
+        pass
