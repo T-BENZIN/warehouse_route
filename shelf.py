b95f35fd7d1f518fee15aa6312f70db2f2e6d4a3
T-BENZIN <tbenzin1337@gmail.com>
2025-04-21 16:55:39 +0500

    + В класс полки добавлены свойства с отдельным перечислением ряда, стеллажа и полки

--- shelf.py
+++ shelf.py
@@ -8,6 +8,10 @@ class Shelf:
         :type address: str
         """
         self.__address = address
+        address_list = address.split(sep='/', maxsplit=2)
+        self.__aisle = int(address_list[0])
+        self.__shelf = address_list[1]
+        self.__rack = int(address_list[2])
         self.__coordinates = self.coordinates_calculation()
         self.__pickup_coordinates = self.pickup_coordinates_calculation()
         self.__upper_passage_y_coordinate, self.__lower_passage_y_coordinate = self.passage_y_coordinates_calculation()
@@ -15,6 +19,18 @@ class Shelf:
     @property
     def address(self) -> str:
         return self.__address
+
+    @property
+    def aisle(self) -> int:
+        return self.__aisle
+
+    @property
+    def shelf(self) -> str:
+        return self.__shelf
+
+    @property
+    def rack(self) -> int:
+        return self.__rack
 
     @property
     def coordinates(self) -> tuple[int, int]:
